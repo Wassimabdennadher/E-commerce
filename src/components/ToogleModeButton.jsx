0c4cfff4d5c5accfe8b4b29a05f8fa346a194631
Wassim Abdennadher <wassim.abd@gmail.com>
2023-05-26 17:00:11 +0100

    set onther css style

--- src/components/ToogleModeButton.jsx
+++ src/components/ToogleModeButton.jsx
@@ -5,7 +5,7 @@ import { ToogleModeContest } from '../context/DarkContext/ToogleMode'
 function ToogleModeButton() {
     const {dark , toogleDark} = useContext(ToogleModeContest)
     return (
-        <div className={`${dark ? 'light' : 'dark'}`}>
+        <div className={`${dark ? 'light' : 'dark'} p-4 `}>
             <input type="checkbox" className={`checkbox`} id="checkbox" />
             <label for="checkbox" className={`checkbox-label ${dark ? 'dark' : 'light'}`}onClick={() => {toogleDark()}}>
                 <i className="fas fa-moon"></i>
