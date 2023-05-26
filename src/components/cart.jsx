167f594b8d287070b135463a2046705424f26d83
Wassim Abdennadher <wassim.abd@gmail.com>
2023-05-26 14:23:01 +0100

    set the total

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -8,7 +8,7 @@ function cart() {
         isOpen ? closeCard() : openCard()
     }
     const total = useMemo(() => {
-        let sumNumber = 0 
+        let sumNumber = 0
         products.map(product => {
             sumNumber = sumNumber + (product.price * (product.quantity + 1))
         })
@@ -16,6 +16,9 @@ function cart() {
     }, [products])
     return (
         <div className={`custom-cart  ${isOpen ? 'trans' : ''}`} >
+            <div className='shopnstop'>
+                <i className="bi bi-cart4 fs-5 "></i>
+            </div>
             <div className='cart_container'>
                 <button className={`custom_button ${!isOpen ? 'custom-close-button' : ''}`} onClick={() => { handleToogleIsOpen() }}  >
                     <i className={`bi bi-${isOpen ? 'x' : 'cart4'}`}></i>
@@ -31,7 +34,9 @@ function cart() {
                 </div>
             </div>
             <div className='total '>
-                <span style={{ color: 'white' }}>Total : {total}</span>
+                <div className='d-flex' >
+                    <span style={{ color: 'white' }}>Total purchased : <b>{total} Dinar(s)</b></span>
+                </div>
             </div>
         </div>
     )
