179a60aa92a9c6ea79c51dbc7053e74ce068bea2
Wassim Abdennadher <wassim.abd@gmail.com>
2023-05-29 15:00:09 +0100

    add some cart changes

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -3,26 +3,24 @@ import useCart from '../context/CartContext/useCart'
 
 function CartProduct(props) {
     const product = props.product
-    const { removeProduct,decerementQuantity,incerementQuantity } = useCart()
+    const { removeProduct, decerementQuantity, incerementQuantity } = useCart()
     return (
-        <div className="card special-cart-product">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src={product.thumbnail} style={{ overflow: 'hidden' }} className="img-fluid rounded-start" alt="..." />
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h6 className="card-title cut-text">{product.title}</h6>
-                        <div className='d-flex justify-content-center align-items-around gap-2'>
-                            <h6 className="card-title">{`Quantity Added : ${product.quantity + 1}`} </h6>
-                                <i className={`bi bi-dash-circle`} onClick={() => {if (product.quantity >= 1){decerementQuantity(product)}}}></i>
-                                <i className="bi bi-plus-circle" onClick={() => incerementQuantity(product)}></i>          
-                        </div>
-                        <h6 className="card-title">{`${product.price * (product.quantity + 1)} Dinar(s)`}</h6>
-                    </div>
-                </div>
+        <div className="special-cart-product ">
+            <div className='image-container-cart'>
+                <img src={product.thumbnail} className="img-fluid rounded-start" style={{ width: '100%', height: '100%' }} alt="..." />
             </div>
-            <i className="bi bi-x fs-3" onClick={() => removeProduct(product)} ></i>
+            <div className="d-flex flex-column justify-content-center  align-items-around p-2">
+                <h6 className="cut-text">{product.title}</h6>
+                <div className='d-flex justify-content-between'>
+                    <h6>{`Quantity Added : ${product.quantity + 1}`} </h6>
+                
+                        <i className={`bi bi-dash-circle`} onClick={() => { if (product.quantity >= 1) { decerementQuantity(product) } }}></i>
+                        <i className="bi bi-plus-circle" onClick={() => incerementQuantity(product)}></i>
+                  
+                </div>
+                <h6 className="card-title">{`${product.price * (product.quantity + 1)} Dinar(s)`}</h6>
+            </div>
+            <i className="bi bi-x fs-3"style={{color:'red'}} onClick={() => removeProduct(product)} ></i>
         </div>
     )
 }
